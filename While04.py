57b403922712df41127529585546012a978e02de
elmurod <elmurodraimov764@gmail.com>
2023-11-16 13:49:02 +0500

    upper

--- While04.py
+++ While04.py
@@ -1,9 +1,9 @@
 def main(s):
-    """
-    A variable of type str is given. Find how many uppercase letters there are and return.
-    Args:
-        s: str
-    Returns:
-        int: return answer
-    """
-    return
+    t=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            t+=1
+        i+=1
+    return t
+print(main("CodeschoolUz"))
