668823089f3a2d0a1aeabd94f9777492800dd21e
elmurod <elmurodraimov764@gmail.com>
2023-11-19 18:41:24 +0500

    code

--- While08.py
+++ While08.py
@@ -1,9 +1,10 @@
 def main(s):
-    """
-    A string of numbers is given. Find how many odd digits there are and return.
-    Args:
-        s: str
-    Returns:
-        int: return answer
-    """
-    return
+    m = 0
+    n = 0
+    while n < len(s):
+        if s[n].isdigit() and int(s[n]) % 2 == 1:
+            m += 1
+        n += 1
+    return m
+print(main("1567534"))
+print(main("3489769"))
